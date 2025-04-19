40a9eaaf5ce19ad1dd593b819aab71654b06f5ad
Julian Dominguez <45999377+Seedname@users.noreply.github.com>
2025-04-19 14:29:52 -0400

    parse data

---
+++ dataset/kaggle/combine_data.py
@@ -0,0 +1,48 @@
+import json
+import pathlib
+from text_unidecode import unidecode
+
+
+def get_movies() -> None:
+    movie_names = {}
+    actor_movies = {}
+
+    for file in (path / "movies").glob("*.txt"):
+        with open(file, "r") as f:
+            lines = f.readlines()
+
+        for line in lines:
+            line: dict = json.loads(line)
+            actor_id = list(line.keys())[0]
+            movie_ids = [int(movie["id"]) for movie in line[actor_id]]
+            actor_movies[int(actor_id)] = movie_ids
+            movies = {int(movie["id"]): unidecode(movie["original_title"]) for movie in line[actor_id]}
+            movie_names |= movies
+
+    with open(path / "actor_movies.json", 'w') as f:
+        json.dump(actor_movies, f)
+    
+    with open(path / "movie_names.json", 'w') as f:
+        json.dump(movie_names, f)
+
+def get_actors() -> None:
+    actor_names = {}
+    for file in (path / "outputs").glob("*.txt"):
+        with open(file, "r") as f:
+            lines = f.readlines()
+
+        for line in lines:
+            line = json.loads(line)
+            for actor in line:
+
+                actor_names[line[actor][0]["id"]] = unidecode(line[actor][0]["name"])
+        
+    with open(path / "actor_names.json", "w") as f:
+        json.dump(actor_names, f)
+
+
+
+if __name__ == "__main__":
+    path = pathlib.Path(__file__).parent
+    # get_movies()
+    get_actors()
