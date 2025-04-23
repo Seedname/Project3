b5e3913159a3baa2e13eb934f301b9f2d5b697d5
cubejod <notblankname@gmail.com>
2025-04-22 20:37:24 -0400

    removed unneeded files, added comments to server

--- backend/server.py
+++ backend/server.py
@@ -30,54 +30,66 @@ def bfs():
     source = int(request.args.get('source'))
     target = int(request.args.get('target'))
 
+    # q args: source, path taken
     q = deque()
     q.append((source, []))
+
+    # seen will contain all seen vertices (actors)
     seen = {source}
-    # source is the current actor, 0 is the distance from source (not needed i think), [] contains the path taken
+
     while q:
         cur = q.popleft()
         for pair in graph[cur[0]]:
             actor = pair[0]
             movie = pair[1]
+            # no need to check seen actors
             if actor in seen:
                 continue
             if actor == target:
+                # update path and return
                 path = cur[1] + [[actor, movie]]
                 return jsonify({'path': path}), 200
             seen.add(actor)
+            # update path and append new actor to q
             q.append((actor, cur[1]+[[actor, movie]]))
 
     return jsonify({'path': []}), 200
 
 
 @app.route('/best-first', methods=['GET'])
-# should be renamed to best_first_search. im not doing it because idk if it will brick something
 def best_first_search():
     """
-    Dijkstra's algorithm endpoint.
+    Best-first search algorithm endpoint.
     Expects 'source' and 'target' as query parameters.
     """
     source = int(request.args.get('source'))
     target = int(request.args.get('target'))
 
-    # if you are having issues running this it is probably this next line!
     target_pop = avg_popularities.get(str(target), 0)
+
     pq = []
+    # pq args: distance, priority (based on popularity), source, path taken
     heapq.heappush(pq, (0, 0, source, []))
+
+    # seen will contain all seen vertices (actors)
     seen = {source}
-    # args: distance, priority (based on popularity), source, path taken
+
     while pq:
         cur = heapq.heappop(pq)
         for trip in graph[cur[2]]:
             actor = trip[0]
             movie = trip[1]
             popularity = trip[2]
+            # no need to check a vertex that was already seen
             if actor in seen:
                 continue
             if actor == target:
+                # update path and return
                 path = cur[3] + [[actor, movie]]
                 return jsonify({'path': path}), 200
             seen.add(actor)
+
+            # abs(target_pop-popularity) is our priority heuristic
             heapq.heappush(pq, (cur[0]+1, abs(target_pop-popularity),
                            actor, cur[3]+[[actor, movie]]))
 
